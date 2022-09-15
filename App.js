90491ed44629000665f2773d3973554976e06609
Android Studio <devandroid@ort>
2022-09-15 08:49:56 -0300

    terminado

--- App.js
+++ App.js
@@ -10,15 +10,11 @@ const App = () => {
   const ONE_SECOND_IN_MS = 1000;
 
   const PATTERN = [
-    1 * ONE_SECOND_IN_MS,
-    2 * ONE_SECOND_IN_MS,
-    3 * ONE_SECOND_IN_MS
+    0.5 * ONE_SECOND_IN_MS,
+    0.5 * ONE_SECOND_IN_MS,
+    0.5 * ONE_SECOND_IN_MS
   ];
 
-  const PATTERN_DESC =
-    Platform.OS === "android"
-      ? "esperar 1s, vibrar 1s, esperar 1s"
-      : "wait 1s, vibrate, wait 2s, vibrate, wait 3s";
 
   return (
     <SafeAreaView style={styles.container}>
@@ -28,18 +24,19 @@ const App = () => {
       </View>
       <Separator />
       {Platform.OS == "android"
-        ? [
+        ? (
+          <>
             <View>
               <Button
                 title="Vibrar 3 segundos"
                 onPress={() => Vibration.vibrate(3 * ONE_SECOND_IN_MS)}
               />
-            </View>,
+            </View>
             
             <Separator />
-          ]
+        </>
+        )
         : null}
-      <Text style={styles.paragraph}>Patron: {PATTERN_DESC}</Text>
       <Button
         title="Vibrar intercalado"
         onPress={() => Vibration.vibrate(PATTERN, true)}
@@ -59,10 +56,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     paddingTop: 44,
-    padding: 8
+    padding: 8,
+    backgroundColor: "#f17df5"
   },
   button: {
-    color: "#f194ff"
+    color: "#36f763"
   },
   separator: {
     marginVertical: 8,
@@ -76,7 +74,7 @@ const styles = StyleSheet.create({
   },
   paragraph: {
     margin: 24,
-    textAlign: "center"
+    textAlign: "center",
   },
 });
 
